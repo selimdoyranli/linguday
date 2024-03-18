ce65e2435a7ab7c65c1ee782160e9efc92b02e1d
selimdoyranli <selimdoyranli@gmail.com>
2024-03-18 21:10:35 +0300

    chore: update nuxt config

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  features: {
+    devLogs: false
+  },
 
   // https://nuxt.com/docs/api/configuration/nuxt-config#srcdir
   srcDir: '',
